2415071fe66167d5d9eb479d328bd425c1f8bf9c
kalaiyazhaki <kalaimoni19@gmail.com>
2022-07-02 18:32:37 +0530

    I have code in single inheritance

---
+++ src/org/com/SingleChildClass.java
@@ -0,0 +1,13 @@
+package org.com;
+
+public class SingleChildClass {
+
+	public void southIndia() {
+		System.out.println("South india:1");
+	}
+
+	public void northIndia() {
+		System.out.println("North india:2");
+	}
+
+}
---
+++ src/org/com/SingleInheritance.java
@@ -0,0 +1,24 @@
+package org.com;
+
+public class SingleInheritance extends SingleChildClass  {
+	
+	private void tamilLanguage() {
+		System.out.println("Tamil language: 25");
+
+	}
+	private void englishLanguage() {
+		System.out.println("English language:15");
+	}
+	private void hindiLanguage() {
+		System.out.println("Hindi language:10");
+		
+	}
+	public static void main(String[] args) {
+		SingleInheritance k = new SingleInheritance();
+		k.tamilLanguage();
+		k.englishLanguage();
+		k.hindiLanguage();
+		k.southIndia();
+		k.northIndia();
+}
+}
